de0f69df5353ee1a74861bb68e08d75eb7517622
¨EhoKira¨ <eduardo57izidorio@gmail.com>
2025-06-10 21:19:44 -0400

    Excluido mains dos algoritmos

--- Algoritmos/binary_search_iterative.c
+++ Algoritmos/binary_search_iterative.c
@@ -15,14 +15,3 @@ int binarySearch(int arr[], int low, int high, int x)
 
     return -1;
 }
-
-int main(void)
-{
-    int arr[] = { 2, 3, 4, 10, 40 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 2;
-    int result = binarySearch(arr, 0, n - 1, x);
-   if(result == -1) printf("Element is not present in array");
-   else printf("Element is present at index %d\n",result);
-
-}
--- Algoritmos/binary_search_recursive.c
+++ Algoritmos/binary_search_recursive.c
@@ -15,14 +15,3 @@ int binarySearch(int arr[], int low, int high, int x)
 
     return -1;
 }
-
-int main()
-{
-    int arr[] = { 2, 3, 4, 10, 40 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 10;
-    int result = binarySearch(arr, 0, n - 1, x);
-    if (result == -1) printf("Element is not present in array");
-    else printf("Element is present at index %d", result);
-    return 0;
-}
